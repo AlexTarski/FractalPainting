f80f5da87791381d1341479f491289f05dc4964a
Alex Tarski <Alex.F.L.O.W@yandex.ru>
2024-12-16 16:46:14 +0300

    ActionTask refactor in DIP manner

--- App/ActionsTask.cs
+++ App/ActionsTask.cs
@@ -7,114 +7,143 @@ using FractalPainting.Infrastructure.Common;
 using FractalPainting.Infrastructure.UiActions;
 using FractalPainting.UI;
 using ImageController = FractalPainting.Infrastructure.Common.AvaloniaImageController;
+using Microsoft.VisualStudio.TestPlatform.ObjectModel.Adapter;
 
 namespace FractalPainting.App;
 
 public class ImageSettingsAction : IUiAction
 {
-	public MenuCategory Category => MenuCategory.Settings;
-	public event EventHandler? CanExecuteChanged;
-	public string Name => "Изображение...";
-	
-	public bool CanExecute(object? parameter)
-	{
-		return true;
-	}
+    public MenuCategory Category => MenuCategory.Settings;
+    public event EventHandler? CanExecuteChanged;
+    public string Name => "Изображение...";
 
-	public async void Execute(object? parameter)
-	{
-		var imageSettings = Services.GetImageSettings();
-		await new SettingsForm(imageSettings).ShowDialog(Services.GetMainWindow());
-		Services.GetImageController().RecreateImage(imageSettings);
-	}
+    private ImageSettings imageSettings;
+    private IImageController imageController;
+    private Func<Window> getMainWindow;
+
+    public ImageSettingsAction(ImageSettings imageSettings, IImageController imageController, Func<Window> getMainWindow)
+    {
+        this.imageSettings = imageSettings;
+        this.imageController = imageController;
+        this.getMainWindow = getMainWindow;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public async void Execute(object? parameter)
+    {
+        await new SettingsForm(imageSettings).ShowDialog(getMainWindow());
+        imageController.RecreateImage(imageSettings);
+    }
 }
 
 public class SaveImageAction : IUiAction
 {
-	public MenuCategory Category => MenuCategory.File;
-	public event EventHandler? CanExecuteChanged;
-	public string Name => "Сохранить...";
+    public MenuCategory Category => MenuCategory.File;
+    public event EventHandler? CanExecuteChanged;
+    public string Name => "Сохранить...";
 
-	public bool CanExecute(object? parameter)
-	{
-		return true;
-	}
+    private Func<Window> getMainWindow;
+    private IImageController imageController;
 
-	public async void Execute(object? settings)
-	{
-		var topLevel = TopLevel.GetTopLevel(Services.GetMainWindow());
-		if (topLevel is null) return;
+    public SaveImageAction(Func<Window> getMainWindow, IImageController imageController)
+    {
+        this.getMainWindow = getMainWindow;
+        this.imageController = imageController;
+    }
 
-		var options = new FilePickerSaveOptions
-		{
-			Title = "Сохранить изображение",
-			SuggestedFileName = "image.bmp",
-		};
-		var saveFile = await topLevel.StorageProvider.SaveFilePickerAsync(options);
-		if (saveFile is not null)
-			Services.GetImageController().SaveImage(saveFile.Path.AbsolutePath);
-	}
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public async void Execute(object? settings)
+    {
+        var topLevel = TopLevel.GetTopLevel(getMainWindow());
+        if (topLevel is null) return;
+
+        var options = new FilePickerSaveOptions
+        {
+            Title = "Сохранить изображение",
+            SuggestedFileName = "image.bmp",
+        };
+        var saveFile = await topLevel.StorageProvider.SaveFilePickerAsync(options);
+        if (saveFile is not null)
+            imageController.SaveImage(saveFile.Path.AbsolutePath);
+    }
 }
 
 public class PaletteSettingsAction : IUiAction
 {
-	public MenuCategory Category => MenuCategory.Settings;
-	public event EventHandler? CanExecuteChanged;
-	public string Name => "Палитра...";
+    public MenuCategory Category => MenuCategory.Settings;
+    public event EventHandler? CanExecuteChanged;
+    public string Name => "Палитра...";
 
-	public bool CanExecute(object? parameter)
-	{
-		return true;
-	}
+    private Func<Window> getMainWindow;
+    private Palette palette;
 
-	public async void Execute(object? parameter)
-	{
-		await new SettingsForm(Services.GetPalette()).ShowDialog(Services.GetMainWindow());
-	}
+    public PaletteSettingsAction(Func<Window> getMainWindow, Palette palette)
+    {
+        this.getMainWindow = getMainWindow;
+        this.palette = palette;
+    }
+
+    public bool CanExecute(object? parameter)
+    {
+        return true;
+    }
+
+    public async void Execute(object? parameter)
+    {
+        await new SettingsForm(palette).ShowDialog(getMainWindow());
+    }
 }
 
 public partial class MainWindow : Window
 {
-	private const int MenuSize = 32;
-	// Контролы из авалонии
-	private Menu? menu;
-	private ImageControl? image;
-	
-	public MainWindow() : this(
-			new IUiAction[]
-			{
-				new SaveImageAction(),
-				new DragonFractalAction(),
-				new KochFractalAction(),
-				new ImageSettingsAction(),
-				new PaletteSettingsAction()
-			}, Services.GetImageController())
-	{
-	}
-	
-	private void InitializeComponent()
-	{
-		AvaloniaXamlLoader.Load(this);
+    private const int MenuSize = 32;
+    // Контролы из авалонии
+    private Menu? menu;
+    private ImageControl? image;
 
-		menu = this.FindNameScope()?.Find<Menu>("Menu");
-		image = this.FindNameScope()?.Find<ImageControl>("Image");
-	}
+    public MainWindow() : this(
+            new IUiAction[]
+            {
+                new SaveImageAction(Services.GetMainWindow, Services.GetImageController()),
+                new DragonFractalAction(),
+                new KochFractalAction(),
+                new ImageSettingsAction(Services.GetImageSettings(), Services.GetImageController(), Services.GetMainWindow),
+                new PaletteSettingsAction(Services.GetMainWindow, Services.GetPalette())
+            }, Services.GetImageController())
+    {
+    }
 
-	public MainWindow(IUiAction[] actions, ImageController imageController) 
-	{
-		InitializeComponent();
-		var imageSettings = CreateSettingsManager().Load().ImageSettings;
-		ClientSize = new Size(imageSettings.Width, imageSettings.Height + MenuSize);
-		menu.ItemsSource = actions.ToMenuItems();
-		Title = "Fractal Painter";
-		Services.SetMainWindow(this);
-		
-		imageController.SetControl(image);
-		imageController.RecreateImage(imageSettings);
-	}
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
 
-	private static SettingsManager CreateSettingsManager()
-	{
-		return new SettingsManager(new XmlObjectSerializer(), new FileBlobStorage());
-	}
+        menu = this.FindNameScope()?.Find<Menu>("Menu");
+        image = this.FindNameScope()?.Find<ImageControl>("Image");
+    }
+
+    public MainWindow(IUiAction[] actions, ImageController imageController)
+    {
+        InitializeComponent();
+        var imageSettings = CreateSettingsManager().Load().ImageSettings;
+        ClientSize = new Size(imageSettings.Width, imageSettings.Height + MenuSize);
+        menu.ItemsSource = actions.ToMenuItems();
+        Title = "Fractal Painter";
+        Services.SetMainWindow(this);
+
+        imageController.SetControl(image);
+        imageController.RecreateImage(imageSettings);
+    }
+
+    private static SettingsManager CreateSettingsManager()
+    {
+        return new SettingsManager(new XmlObjectSerializer(), new FileBlobStorage());
+    }
 }
